52f2a1bb295217b0b02a54a1bc9cf8c9455b88c5
Apoorva Jain <apoorva.jain3293@gmail.com>
2019-09-23 22:57:13 +0530

    Added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # Callbacks
+  before_create :set_username
   after_create :send_successful_registration_email
 
   # Validations
@@ -10,10 +11,17 @@ class User < ApplicationRecord
     message: 'Only valid emails allowed'
   }
 
+  validates :password_confirmation, presence: true, on: :create
   validates :password, confirmation: true, presence: true,
                        length: { minimum: 8 }, on: :create
 
+  validates :username, length: { minimum: 5 }, on: :update
+
   private
+
+  def set_username
+  	self.username = self.email.split('@').first
+  end
 
   def send_successful_registration_email
   	UserMailer.registration_email(self).deliver_now
