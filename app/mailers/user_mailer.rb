0d5b1485f890ad763bff4c95971cb20ec96951e4
Apoorva Jain <apoorva.jain3293@gmail.com>
2019-09-25 18:18:21 +0530

    Password reset mailer and email layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,9 @@ class UserMailer < ApplicationMailer
 		@user = user
 		mail(to: user.email, subject: 'Welcome')
 	end
+
+	def password_reset(user)
+    @user = user
+    mail(:to => user.email, :subject => 'Password Reset')
+  end
 end
