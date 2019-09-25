6ecf054519a0fee16512fff8b725b61eb235b416
Apoorva Jain <apoorva.jain3293@gmail.com>
2019-09-25 18:19:26 +0530

    Password reset routes and config settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
   resources :users
 
   resources :sessions, :only => [:create]
+
+  resources :password_resets
+  
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
