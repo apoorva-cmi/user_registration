0f2f478afcc30f63f88b69d6e29a4eb1afda4dad
Apoorva Jain <apoorva.jain3293@gmail.com>
2019-09-25 18:13:47 +0530

    Add password reset to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190923095149) do
+ActiveRecord::Schema.define(version: 20190924140142) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20190923095149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
